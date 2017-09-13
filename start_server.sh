5a69b67f5754f7ab16dc9d8f704dcd5ff056074e
Max Kusnadi <max.kusnadi@gmail.com>
2017-09-14 02:28:43 +0800

    change server port

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git pull
 kill $(ps aux | grep 'assignment-3' | awk '{print $2}')
-nohup gunicorn --certfile cert.pem --keyfile key.pem -k gevent -b 0.0.0.0:8080 --worker-connections 10 wsgi:app &
+nohup gunicorn --certfile cert.pem --keyfile key.pem -k gevent -b 0.0.0.0:3000 --worker-connections 10 wsgi:app &
